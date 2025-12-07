268b5cdf2362778a061685554c5decba86a579b4
zeelepton-ops <zeelepton@gmail.com>
2025-12-07 16:08:03 +0300

    Fix ESLint configuration for TypeScript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   root: true,
   extends: ['next/core-web-vitals'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
-    'react/no-unescaped-entities': 'off'
+    'react/no-unescaped-entities': 'off',
+    'react-hooks/exhaustive-deps': 'warn'
   }
 }
